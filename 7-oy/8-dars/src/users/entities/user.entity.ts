bc195faa9cf83a2a535db5a37ae487e3080fe593
hurshidbe <egamberdiyevx8@gmail.com>
2025-04-30 18:27:38 +0500

    lohiha davom etmoqd a

--- 7-oy/8-dars/src/users/entities/user.entity.ts
+++ 7-oy/8-dars/src/users/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('rowid')
+  id: number;
+
+  @Column({ type: 'varchar' })
+  name: string;
+
+  @Column({ type: 'varchar' })
+  email: string;
+
+  @Column({ type: 'varchar' })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
